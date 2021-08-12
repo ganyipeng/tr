3473c16759ca72e3971ebbc3e2a2bf06cb551814
甘宜鹏 <981057040@qq.com>
2021-08-12 16:15:10 +0800

    黄同学修改表格线太细或图片太小，导致的文字识别不出来的问题

--- getVerticalBorder.py
+++ getVerticalBorder.py
@@ -32,6 +32,16 @@ def erode_img(img,kernel_args=(2,2),iterations=1):
     kernel = numpy.ones(kernel_args, numpy.uint8)
     return cv2.erode(img, kernel,iterations=iterations)
 
+def dilate_img(img, kernal_args:tuple, iterations:int):
+    """
+    dilate image
+    @param kernel_args 卷积核参数（2，2）
+    @param interations dilate的迭代次数
+    """
+
+    kernel = numpy.ones(kernal_args, numpy.uint8)
+    return cv2.dilate(img, kernel,iterations=iterations)
+
 def get_row_x(img):
     """
     先对图片做各种变换，使图片只剩表格的竖线，然后返回竖线的x坐标
@@ -39,8 +49,11 @@ def get_row_x(img):
     img = gray_img(img)
     img = bin_img(img)
     img = erode_img(img, (2, 1), 40)
+    img = dilate_img(img, (2, 2), 1)
+    cv2.imshow("", img)
+    cv2.waitKey(0)
     x_dict = {} # 对很多行进行取样，统计在竖线上的x坐标
-    for x in range(0, img.shape[0], 100):  #每隔100个像素点进行一行取样
+    for x in range(0, img.shape[0], 50):  #每隔50个像素点进行一行取样
         for y in range(img.shape[1]):  # 遍历这一行
             if img[x][y] > 100: # 如果遇到了竖线
                 flag = True
@@ -52,6 +65,7 @@ def get_row_x(img):
                     x_dict[y] = 1
     row_x = [] # 统计在竖线上的x坐标
     for key in x_dict.keys():
-        if x_dict[key] > img.shape[0]/100 * 1.5: #如果超过取样数的1.5倍，则说明这个x坐标是竖线的
+        if x_dict[key] > img.shape[0]/50 * 1.5: #如果超过取样数的1.5倍，则说明这个x坐标是竖线的
             row_x.append(key)
+
     return row_x
