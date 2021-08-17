8bfcb210e34af6da1abaf7a89bfc1332e41ad3d4
ganyipeng <981057040@qq.com>
2021-08-17 16:41:46 +0800

    add filter_neighbor function and ignore horizontal line

--- getVerticalBorder.py
+++ getVerticalBorder.py
@@ -43,6 +43,22 @@ def dilate_img(img, kernal_args:tuple, iterations:int):
     kernel = numpy.ones(kernal_args, numpy.uint8)
     return cv2.dilate(img, kernel,iterations=iterations)
 
+def filter_neighbor(row):
+    # row = [7, 17, 440, 450, 814, 824, 1188, 1198]
+    r = []
+    i = 0
+    row_len = len(row)
+    while i < row_len:
+        r.append(row[i])
+        if i == row_len - 1:
+            break
+        if row[i+1] - row[i] < 15:
+            i += 1
+        i += 1
+    # print('before', row)
+    # print('after', r)
+    return r
+
 def get_row_x(img):
     """
     先对图片做各种变换，使图片只剩表格的竖线，然后返回竖线的x坐标
@@ -54,14 +70,19 @@ def get_row_x(img):
     # cv2.imshow("", img)
     # cv2.waitKey(0)
     x_dict = {} # 对很多行进行取样，统计在竖线上的x坐标
+    column_count = img.shape[1]
     for x in range(0, img.shape[0], 50):  #每隔50个像素点进行一行取样
-        for y in range(img.shape[1]):  # 遍历这一行
+        # if too many 1: it's horizontal line continue
+        if numpy.sum(img[x] > 100) > column_count/2:
+            continue
+        for y in range(column_count):  # 遍历这一行
             if img[x][y] > 100: # 如果遇到了竖线
                 flag = True
                 for key in x_dict.keys():
                     if abs(y-key) < 10: #如果这条竖线上已经有x坐标被找到了，就在那个x坐标的统计上加一
                         x_dict[key]+=1
                         flag = False
+                        break
                 if flag: # 如果这条竖线上还没有 x 坐标被找到，就加一个新的项
                     x_dict[y] = 1
     row_x = [] # 统计在竖线上的x坐标
@@ -71,4 +92,6 @@ def get_row_x(img):
     print('row_x:::',row_x)
     row_x.sort()
     print('row_x_sort:::',row_x)
+    row_x = filter_neighbor(row_x)
+    print('row_x_filter:::',row_x)
     return row_x
